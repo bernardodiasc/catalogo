319a4c06f10966f0246b43221a71952308a42dab
Bernardo Dias <bernardodiasdacruz@gmail.com>
2018-02-20 18:11:39 -0300

    Updates

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,9 +4,9 @@ import { storiesOf } from '@storybook/react'
 import Docs from 'stories/containers/Docs'
 
 import AppReadme from '../../README.md'
-import DevelopmentReadme from '../../docs/development.md'
-import StorybookReadme from '../../docs/storybook.md'
-import ContentReadme from '../../docs/content.md'
+import DevelopmentReadme from '../../documentation/development.md'
+import StorybookReadme from '../../documentation/storybook.md'
+import ContentReadme from '../../documentation/content.md'
 storiesOf('Getting started', module)
   .add('Project instructions', () => <Docs>{AppReadme}</Docs>)
   .add('Development instructions', () => <Docs>{DevelopmentReadme}</Docs>)
